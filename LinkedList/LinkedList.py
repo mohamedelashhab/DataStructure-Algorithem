19701d0353276bd310c8945c4575df474f1e9e41
Mohamed ahmed <elashhab_fcih@yahoo.com>
2020-08-06 01:24:20 +0200

    LinkedList

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,96 @@
+class LinkedList:
+    def __init__(self):
+        self.__first = None
+        self.__last = None
+        self.__size = 0
+
+    class __Node:
+        def __init__(self, value:int):
+            self.__value = value
+            self.__next = None
+
+    def addLast(self, item: int):
+        node = self.__Node(item)
+        if self.__isEmpty():
+            self.__first = self.__last = node
+        else:
+            self.__last._Node__next = node
+            self.__last = node
+        self.__size+=1
+
+    def addFirst(self, item: int):
+        node = self.__Node(item)
+        if self.__isEmpty():
+            self.__first = self.__last = node
+        else:
+            node._Node__next = self.__first
+            self.__first = node
+        self.__size += 1
+
+    def indexOf(self, item):
+        index = 0
+        current = self.__first
+        while (current != None):
+            print(current._Node__value)
+
+            if current._Node__value == item:
+                return index
+            else:
+                index += 1
+                current = current._Node__next
+        return -1
+
+    def contains(self, item: int):
+        return  self.indexOf(item) != -1
+
+    def removeFirst(self):
+        if self.__isEmpty():
+            raise IndexError
+
+
+        if self.__first == self.__last:
+            self.__first = self.__last = None
+        else:
+            second = self.__first._Node__next
+            self.__first._Node__next = None
+            self.__first = second
+
+        self.__size -= 1
+
+    def removeLast(self):
+        if self.__isEmpty():
+            raise IndexError
+
+        if self.__first == self.__last:
+            self.__first = self.__last = None
+        else:
+            previous = self.getPrevious(self.__last)
+            self.__last = previous
+            self.__last._Node__next = None
+        self.__size -= 1
+
+
+
+    def getPrevious(self, node):
+        current = self.__first
+        while (current != None):
+            if current._Node__next == node: return current
+            current = current._Node__next
+        return None
+
+    def toArray(self):
+        arr = []
+        current = self.__first
+        while current != None:
+            arr.append(current._Node__value)
+            current = current._Node__next
+        return arr
+
+
+    def __isEmpty(self):
+        return self.__first is None
+
+    def size(self):
+        return self.__size
+
+
---
+++ LinkedList/Main.py
@@ -0,0 +1,20 @@
+from LinkedList import LinkedList
+
+if __name__ == "__main__":
+    list = LinkedList()
+    list.addLast(10)
+    list.addLast(20)
+    list.addLast(30)
+    list.addLast(40)
+    list.addLast(50)
+    print(list.size())
+    # list.removeFirst()
+    print(list.size())
+
+
+    print(list.size())
+
+    print(list.toArray())
+
+
+
